b44ffd07b5e4d0191d78d94793637d09b1868e4f
arsen1tn <arsen.tadevosian749@gmail.com>
2026-05-30 17:19:43 +0200

    impl. OLLAMA and answer

--- AXON.py
+++ AXON.py
@@ -7,3 +7,20 @@ app = FastAPI()
 def chat(data: dict):
     
     user_message = data["message"]
+    response = requests.post(
+        "http://localhost:11434/api/chat", json={"model": "qwen2.5:3b", "messages": [
+            {
+                "role": "user", "content": user_message
+                }
+            ]
+                                                 }
+        )
+    
+    result = response.json()
+    
+    ai_response = result["message"]["content"]
+    
+    return {
+        "response": ai_response
+        }
+    
