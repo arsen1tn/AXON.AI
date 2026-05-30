fad6e4eb4c650c293e593934079540e12708e520
arsen1tn <arsen.tadevosian749@gmail.com>
2026-05-30 19:42:05 +0200

    impl. memory(1chat = 1 memory, restart = memory lost)

--- AXON.py
+++ AXON.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 import requests
 
+messages = []
+
 app = FastAPI()
 
  # USER MESSAGE
@@ -8,12 +10,12 @@ app = FastAPI()
 @app.post("/chat")
 def chat(data: dict):
     user_message = data["message"]
+    messages.append({
+        "role": "user", "content": user_message
+        })
+    
     response = requests.post(
-        "http://localhost:11434/api/chat", json={"model": "qwen2.5:3b", "messages": [
-            {
-                "role": "user", "content": user_message
-                }
-            ],
+        "http://localhost:11434/api/chat", json={"model": "qwen2.5:3b", "messages": messages,
             "stream": False
                                                  }
         )
@@ -24,7 +26,11 @@ def chat(data: dict):
     
     ai_response = result["message"]["content"]
     
+    messages.append({
+        "role": "assistant", "content": ai_response
+        })
+    
     return {
         "response": ai_response
         }
-    
+
