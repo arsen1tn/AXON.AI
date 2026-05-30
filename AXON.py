62b8a0c0c4198bc935fc8601f000dc68b7e11eaf
arsen1tn <arsen.tadevosian749@gmail.com>
2026-05-30 17:47:59 +0200

    responce from AI. WORKING VERSION 0.1

--- AXON.py
+++ AXON.py
@@ -3,19 +3,23 @@ import requests
 
 app = FastAPI()
 
+ # USER MESSAGE
+ 
 @app.post("/chat")
 def chat(data: dict):
-    
     user_message = data["message"]
     response = requests.post(
         "http://localhost:11434/api/chat", json={"model": "qwen2.5:3b", "messages": [
             {
                 "role": "user", "content": user_message
                 }
-            ]
+            ],
+            "stream": False
                                                  }
         )
-    
+
+# RESPONSE FROM AI MODEL
+   
     result = response.json()
     
     ai_response = result["message"]["content"]
